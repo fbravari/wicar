fab67d8864b1bf1164299101f09756434e9e1ae1
fbravari <francisco.m@rompecabeza.cl>
2023-03-16 14:06:56 -0300

    Commit include footer

--- terminos-y-condiciones.php
+++ terminos-y-condiciones.php
@@ -12,56 +12,7 @@
 
     <?php include("template-parts/header.php"); ?>
     <?php include("template-parts/terminos.php"); ?>
-    <footer class="bg-black py-4">
-        <div class="container py-4">
-            <div class="row">
-                <div class="col-12 col-md-3">
-                    <img src="assets/img/wicar.svg" alt="Wicar" class="img-fluid mb-4 pb-4" />
-                </div>
-                <div class="col-12 col-md-3">
-                    <div class="w-100 pb-3">
-                        <h4 class="text-white">Hablemos</h4>
-                        <ul>
-                            <li><a href="mailto:contacto@wicar.cl">contacto@wicar.cl</a></li>
-                            <li><a href="tel:+569 8399 7341">+569 8399 7341</a></li>
-                            <li>Lunes a viernes de <br/>08:30 a 17:30 horas</li>
-                        </ul>
-                    </div>
-                    <div class="w-100 pb-3">
-                        <h4 class="text-white">Emergencias</h4>
-                        <ul>
-                            <li>Marca *2400 desde cualquier teléfono</li>
-                            <li>Atención las 24 horas</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-12 col-md-3">
-                    <div class="w-100 pb-3">
-                        <h4 class="text-white">Explora</h4>
-                        <ul>
-                            <li><a href="#">Terminos y condiciones</a></li>
-                            <li><a href="#">Preguntas frecuentes</a></li>
-                        </ul>
-                    </div>
-                    <div class="w-100 pb-3">
-                        <h4 class="text-white">Recursos</h4>
-                        <ul>
-                        <li><a href="#">Manual de usuario</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-12 col-md-3">
-                    <div class="w-100 pb-3">
-                        <h4 class="text-white">Síguenos en</h4>
-                        <div class="d-flex justify-content-start align-items-center">
-                            <a href="#" class="mr-4"><img src="assets/img/icons8-facebook.svg" alt="Facebook" /></a>
-                            <a href="#"><img src="assets/img/Icon-awesome-instagram.svg" alt="Instagram" /></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php include("template-parts/footer.php"); ?>
 
     
     </body>
