f2c3acabc3310db9790e078cd8f702fa7e408f3d
JennyVicencioDev <jennyfer.v@rompecabeza.cl>
2023-03-16 13:25:59 -0300

    arreglo clase logo footer

--- terminos-y-condiciones.php
+++ terminos-y-condiciones.php
@@ -16,7 +16,7 @@
         <div class="container py-4">
             <div class="row">
                 <div class="col-12 col-md-3">
-                    <img src="assets/img/wicar.svg" alt="Wicar" class="mb-4 pb-4" />
+                    <img src="assets/img/wicar.svg" alt="Wicar" class="img-fluid mb-4 pb-4" />
                 </div>
                 <div class="col-12 col-md-3">
                     <div class="w-100 pb-3">
