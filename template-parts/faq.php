db47f1b4beddcc65d791e74f122111addd2a05e1
JennyVicencioDev <jennyfer.v@rompecabeza.cl>
2023-03-16 15:21:32 -0300

    preguntas actualizadas

--- template-parts/faq.php
+++ template-parts/faq.php
@@ -4,25 +4,167 @@
         <div class="row">
             <div class="col-12">
                 <div class="accordion accordion-faq" id="wicar-faq">
-                    <?php 
-                    $counter = 1;
-                    for ($i=1; $i<=10; $i++) { ?>
-                        <div class="accordion-item">
-                          <h2 class="accordion-header mb-0">
-                            <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-<?php echo $counter; ?>" aria-expanded="true" aria-controls="faq-<?php echo $counter; ?>">
-                              ¿Qué es Wicar?
-                            </button>
-                          </h2>
-                          <div id="faq-<?php echo $counter; ?>" class="accordion-collapse collapse" data-parent="#wicar-faq">
-                            <div class="accordion-body">
-                              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero placeat id distinctio pariatur numquam temporibus minus culpa laboriosam commodi facilis. Assumenda reiciendis explicabo aspernatur ex omnis amet odio magni dolores.
-                            </div>
-                          </div>
-                        </div>
-                    <?php 
-                    $counter++;
-                    }
-                    ?>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header mb-0">
+                      <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-1" aria-expanded="true" aria-controls="faq-1">
+                        ¿Qué es Wicar?
+                      </button>
+                    </h2>
+                    <div id="faq-1" class="accordion-collapse collapse" data-parent="#wicar-faq">
+                      <div class="accordion-body">
+                        WiCar es una app de seguridad y asistencia para automóviles. Revisa aquí todo lo que WiCar puede hacer para entregarte la seguridad que necesitas.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header mb-0">
+                      <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-2" aria-expanded="true" aria-controls="faq-2">
+                        ¿Cuánto cuesta Wicar?
+                      </button>
+                    </h2>
+                    <div id="faq-2" class="accordion-collapse collapse" data-parent="#wicar-faq">
+                      <div class="accordion-body">
+                      El precio de lanzamiento de WiCar es de $320.000 (Iva inc.) por el primer año de servicio. A partir del segundo año, la cuota anual es de $120.000 (Iva inc.).
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header mb-0">
+                      <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-3" aria-expanded="true" aria-controls="faq-3">
+                        ¿Cuánto demora la instalación de Wicar en mi auto?
+                      </button>
+                    </h2>
+                    <div id="faq-3" class="accordion-collapse collapse" data-parent="#wicar-faq">
+                      <div class="accordion-body">
+                        La instalación tarda una hora y media aproximadamente.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header mb-0">
+                      <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-4" aria-expanded="true" aria-controls="faq-4">
+                        ¿Puedo instalar Wicar en más de un celular?
+                      </button>
+                    </h2>
+                    <div id="faq-4" class="accordion-collapse collapse" data-parent="#wicar-faq">
+                      <div class="accordion-body">
+                        Sí, cada cuenta de WiCar tiene 4 usuarios disponibles.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header mb-0">
+                      <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-5" aria-expanded="true" aria-controls="faq-5">
+                        ¿Qué pasa si tengo Block Assist?
+                      </button>
+                    </h2>
+                    <div id="faq-5" class="accordion-collapse collapse" data-parent="#wicar-faq">
+                      <div class="accordion-body">
+                        <ul>
+                          <li>En caso de tener el sistema Block Assist, debes realizar la compra de la membresía de primer año por $289.990.</li>
+                          <li>El sistema Block Assist no es compatible con la nueva tecnología de WiCar por lo que es necesario retirar los dispositivos para instalar los de WiCar.</li>
+                          <li>La ventaja está en términos de conectividad, dado que usamos tecnología de punta, asegurando mayor señal y comunicación entre la plataforma y los dispositivos del auto.</li>
+                          <li>Además, contamos con una mayor prestación de servicios, como por ejemplo el SOS para emergencias y el sistema de recuperación de autos robados.</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header mb-0">
+                      <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-6" aria-expanded="true" aria-controls="faq-6">
+                        ¿Dónde compro Wicar?
+                      </button>
+                    </h2>
+                    <div id="faq-6" class="accordion-collapse collapse" data-parent="#wicar-faq">
+                      <div class="accordion-body">
+                        Puedes comprarlo en nuestra web o en cualquier sucursal Kaufmann a lo largo de todo el país. Recomendamos el pago web, ya que esto nos permite coordinar las horas de taller para hacer tu experiencia mucho más fluida.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header mb-0">
+                      <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-7" aria-expanded="true" aria-controls="faq-7">
+                        ¿Puedo bloquear mi auto en movimiento?
+                      </button>
+                    </h2>
+                    <div id="faq-7" class="accordion-collapse collapse" data-parent="#wicar-faq">
+                      <div class="accordion-body">
+                        Sí, puedes bloquear tu auto en movimiento. Sin embargo en WiCar, empresa el Grupo Kaufmann, lo primero es tu seguridad. Por esta razón, el bloqueo de motor se hace efectivo una vez que el vehículo se haya detenido completamente y el motor se encuentre apagado.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header mb-0">
+                      <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-8" aria-expanded="true" aria-controls="faq-8">
+                        ¿Cómo funciona el bloqueo de motor?
+                      </button>
+                    </h2>
+                    <div id="faq-8" class="accordion-collapse collapse" data-parent="#wicar-faq">
+                      <div class="accordion-body">
+                        El bloqueo de motor o inmovilizador del automóvil es un sistema que desconecta temporalmente algunos sistemas de tu vehículo garantizando que este no pueda ser encendido posterior a realizar el bloqueo en la aplicación móvil.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header mb-0">
+                      <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-9" aria-expanded="true" aria-controls="faq-9">
+                        ¿Puedo bloquear el motor en cualquier mmomento?
+                      </button>
+                    </h2>
+                    <div id="faq-9" class="accordion-collapse collapse" data-parent="#wicar-faq">
+                      <div class="accordion-body">
+                        Recomendamos hacer uso del bloqueo de motor única y exclusivamente en caso de ser víctima del robo del automóvil. Recuerda que si bloqueas tu automóvil y este va en movimiento, el bloqueo se hará efectivo en el siguiente encendido.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header mb-0">
+                      <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-10" aria-expanded="true" aria-controls="faq-10">
+                        ¿Cómo se utiliza el bloqueo de motor?
+                      </button>
+                    </h2>
+                    <div id="faq-10" class="accordion-collapse collapse" data-parent="#wicar-faq">
+                      <div class="accordion-body">
+                        El uso del bloqueo de motor se recomienda exclusivamente para el caso de ser víctima de robo del automóvil. Si dejas tu auto estacionado en algún lugar y quieres bloquear el motor, puedes hacerlo. Sin embargo recomendamos no prolongar el bloqueo de motor por más de 24 horas seguidas.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header mb-0">
+                      <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-11" aria-expanded="true" aria-controls="faq-11">
+                        ¿El sistema Wicar puede descargar la batería de mi auto?
+                      </button>
+                    </h2>
+                    <div id="faq-11" class="accordion-collapse collapse" data-parent="#wicar-faq">
+                      <div class="accordion-body">
+                        En condiciones normales, WiCar no descarga la batería de tu automóvil. Sin embargo, si se bloquea el motor por un tiempo prolongado (más de 24 horas) y tu batería no está en buenas condiciones, esta se podría descargar. Se recomienda tener una batería en buenas condiciones para el correcto funcionamiento del automóvil.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header mb-0">
+                      <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-12" aria-expanded="true" aria-controls="faq-12">
+                        ¿Wicar es un seguro?
+                      </button>
+                    </h2>
+                    <div id="faq-12" class="accordion-collapse collapse" data-parent="#wicar-faq">
+                      <div class="accordion-body">
+                        No, WiCar no es un seguro. WiCar te ayuda si sufriste algún inconveniente leve en tu camino o si eres víctima de un robo, portonazo o encerrona, teniendo la posibilidad de bloquear el motor, rastrear la ubicación del auto por GPS y comunicarte directamente con autoridades con el botón SOS.
+                      </div>
+                    </div>
+                  </div>
+                  <div class="accordion-item">
+                    <h2 class="accordion-header mb-0">
+                      <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#faq-13" aria-expanded="true" aria-controls="faq-13">
+                        ¿Los servicios de Wicar estás disponibles en todo Chile?
+                      </button>
+                    </h2>
+                    <div id="faq-13" class="accordion-collapse collapse" data-parent="#wicar-faq">
+                      <div class="accordion-body">
+                        Sí, puedes solicitar cualquiera de los servicios a lo largo del país.
+                      </div>
+                    </div>
+                  </div>
                 </div>
             </div>
         </div>
