bdcb68c2dbdc11f806dd87ed41a50733a30bc679
JennyVicencio <jennyfer.v@rompecabeza.cl>
2023-03-15 22:40:16 -0300

    plantillas landings términos y faq

---
+++ template-parts/terminos.php
@@ -0,0 +1,6 @@
+<section class="terminos-condiciones py-2 py-md-4">
+    <div class="container py-2 py-md-4">
+        <h2 class="text-orange">Términos y condiciones</h2>
+
+    </div>
+</section>
---
+++ terminos-y-condiciones.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Wicar</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/owlcarousel/assets/owl.carousel.min.css">
+        <link rel="stylesheet" href="assets/owlcarousel/assets/owl.theme.default.min.css">
+        <link rel="stylesheet" href="assets/css/style-wicar.css">
+    </head>
+    <body>
+
+    <?php include("template-parts/header.php"); ?>
+    <?php include("template-parts/terminos.php"); ?>
+
+    
+    </body>
+    <script
+  src="https://code.jquery.com/jquery-3.6.4.js"
+  integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E="
+  crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="assets/owlcarousel/owl.carousel.min.js"></script>
+    <script src="assets/js/funciones-wicar.js"></script>
+</html>
