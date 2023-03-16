1fb4ac5c712fee90e164347585b955d12e984df1
JennyVicencioDev <jennyfer.v@rompecabeza.cl>
2023-03-16 13:22:50 -0300

    arreglos tags titulos

--- template-parts/terminos.php
+++ template-parts/terminos.php
@@ -4,13 +4,13 @@
         <div class="row">
             <!-- Antecedentes -->
             <div class="col-12">
-                <h3>ANTECEDENTES
-                </h3>
+                <h2>ANTECEDENTES
+                </h2>
                 <p>WICAR es una aplicación que tiene por objeto brindar a los usuarios registrados, asistencia en ruta a sus vehículos inscritos; recordatorios y notificaciones relacionados con dichos, información sobre sus desplazamientos y la posibilidad de facilitar el aviso de robo del vehículo y conectar a servicios de asistencia, a través de la aplicación MOK (grupo MOK), además de permitir a los usuarios acceder a una serie de beneficios adicionales</p>
             </div>
             <!-- Condiciones de uso -->
             <div class="col-12">
-                <h3>Condiciones de uso</h3>
+                <h2>Condiciones de uso</h2>
                 <ol class="pl-3">
                     <li>Una vez aceptados los presentes Términos y Condiciones, el usuario deberá registrar sus datos y crear su contraseña.<br>
                     El usuario deberá validar su cuenta e ingresar los datos de cada uno de los vehículos respecto los cuales usará la aplicación. El usuario es el único responsable de mantener actualizados sus datos.</li>
@@ -24,7 +24,7 @@
             </div>
             <!-- Precio y forma de pago -->
             <div class="col-12">
-                <h3>Precio y forma de pago</h3>
+                <h2>Precio y forma de pago</h2>
                 <p>
                 Precio Suscripción Primer Año: $319.990 CLP IVA incluido.<br>
                 Precio Mantención Servicio Anual: Desde el segundo año en adelante: $120.000 CLP IVA incluido.<<br>
@@ -33,31 +33,31 @@
             </div>
             <!-- Servicios adicionales -->
             <div class="col-12">
-                <h3>Servicios Adicionales</h3>
+                <h2>Servicios Adicionales</h2>
                 <p>El usuario declara conocer y aceptar que una vez que se haya registrado para el uso de la aplicación, recibirá ofertas sobre servicios adicionales que podrá contratar a través de la plataforma WICAR. Cada uno de dichos servicios u ofertas se sujetarán a las condiciones de compra o prestación de servicios aplicable a cada uno de ellos en particular. Los servicios adicionales, permitirán al usuario registrado, contratar asistencia adicional en ruta, la adquisición de accesorios para automóviles, contratación de seguros, oferta de compra de vehículos en caso de venta, acceso a la aplicación AWTO, entre otras.</p>
                 <p>Los servicios que se ofrezcan a través de WICAR, serán prestados por terceros. KAUFMANN solo cumple el rol de intermediario para facilitar el uso y acceso de la plataforma, según se establece en el número 5 de las CONDICIONES DE USO y no asume responsabilidades respecto de los bienes ofrecidos o servicios prestados.</p>
                 <p>Asimismo, el usuario declara conocer que la autorización de uso de datos personales, otorgada en el presente instrumento, permite el envío de ofertas sobre productos o servicios, a través de la plataforma WICAR.</p>
             </div>
             <!-- Restricciones -->
             <div class="col-12">
-                <h3>Restricciones</h3>
+                <h2>Restricciones</h2>
                 <p>El usuario reconoce que KAUFMANN es propietaria y/o licenciataria de los derechos intelectuales sobre la aplicación, y software. Por ende, el usuario se obliga a respetar dicha titularidad, licencia, derechos e intereses no pudiendo en ningún caso reclamar derecho de propiedad alguna sobre los mismos. El usuario se obliga a no replicar o distribuir dicho software o bien utilizar el mismo para propósitos distintos a su propia actividad. El usuario no podrá vender, licenciar, ceder o de alguno otro modo transferir, o copiar todo o parte del software o los servicios como así tampoco podrá hacerlo para sus aliados o subsidiarias o para algún tercero. El usuario no podrá alterar, decodificar, descompilar, desmontar con procesos inversos o reproducir ningún aspecto del software a menos que le estuviere legalmente permitido. Tampoco podrá copiar, revender, ceder, transferir o de algún otro modo proporcionar acceso a ningún tercero. La información y contenido de los servicios (incluyendo, pero sin limitarse a ello, mensajes, información, datos, textos, sonidos, fotos, gráficos, videos, mapas, iconos, software, códigos o cualquier otro material), así como la infraestructura utilizada para proveer dicho contenido e información, es propiedad de KAUFMANN. El usuario acepta no modificar, copiar, distribuir, transmitir, mostrar, reproducir, publicar, otorgar licencias, transferir, vender o revender ninguna información, software, productos o servicios obtenidos desde o a través de los servicios ofrecidos.</p>
             </div>
             <!-- Obligaciones del usuario -->
             <div class="col-12">
-                <h3>Obligaciones del usuario</h3>
+                <h2>Obligaciones del usuario</h2>
                 <p>La aplicación WICAR no está disponible para el uso de personas menores de 18 años. El usuario no podrá autorizar a terceros a utilizar su cuenta, asimismo no podrá ceder o transferir de otro modo su cuenta a cualquier otra persona o entidad. El usuario se obliga a cumplir con todas las leyes aplicables al utilizar la aplicación y solo podrá utilizarla con fines legítimos.</p>
                 <p>El usuario acuerda no proporcionar contenido de usuario que sea difamatorio, calumnioso, odioso, violento, obsceno, pornográfico, ilícito o de otro modo ofensivo, como determine KAUFMANN, a su sola discreción, tanto si dicho material pueda estar protegido o no por la ley. KAUFMANN podrá, a su sola discreción y en cualquier momento y por cualquier motivo, sin avisarle previamente, revisar, controlar o eliminar contenido de usuario, pero sin estar obligada a ello.</p>
             </div>
             <!-- Autorización uso datos -->
             <div class="col-12">
-                <h3>Autorización uso datos personales</h3>
+                <h2>Autorización uso datos personales</h2>
                 <p>Para efectos de lo dispuesto en la Ley N° 19.628 sobre protección de datos de carácter personal, el usuario autoriza expresamente a KAUFMANN para almacenar, conservar, custodiar y transmitir a registros o bancos de datos personales, la información proporcionada, con el objeto de que sea utilizada por aquellas entidades relacionadas con la prestación de los servicios ofrecidos por la aplicación. Asimismo, el usuario autoriza a KAUFMANN para que tome contacto con él a través de correos electrónicos y mensajes de texto (SMS) enviados a su teléfono móvil particular, con el objeto de poner en su conocimiento distintos aspectos relativos al funcionamiento de la aplicación.</p>
                 <p>Así mismo, cualquier información recolectada desde el usuario por KAUFMANN, ya sea a través del uso de la aplicación móvil, uso de su sitio web, llamadas telefónicas, correo electrónico, y en general cualquier medio de comunicación sea digital o no, KAUFMANN se reserva el derecho de darle uso a esa información ya sea para fines de mejorar o dar continuidad al servicio.</p>
             </div>
             <!-- Liberación de responsabilidad -->
             <div class="col-12">
-                <h3>Liberación de repsonsabilidad</h3>
+                <h2>Liberación de repsonsabilidad</h2>
                 <p>KAUFMANN queda expresamente liberada de toda obligación y responsabilidad para con el usuario, en los siguientes casos:</p>
                 <ol style="list-style: lower-alpha;">
                     <li>Mal uso por parte del usuario o de terceros, del dispositivo o de la aplicación o uso de la misma para fines distintos de los contratados.</li>
@@ -68,13 +68,13 @@
             </div>
             <!-- Horario atención -->
             <div class="col-12">
-                <h3>Horario de atención remota</h3>
+                <h2>Horario de atención remota</h2>
                 <p>En horarios de oficina de Lunes a Viernes de 09.00 a 18.00 hrs.</p>
                 <p>Correo de contacto: <a href="contacto@wicar.cl" class="text-orange" target="_blank">contacto@wicar.cl</a></p>
             </div>
             <!-- Término de contrato -->
             <div class="col-12">
-                <h3>Término de contrato</h3>
+                <h2>Término de contrato</h2>
                 <p>El presente contrato tendrá una duración indefinida a contar de la fecha de la aceptación de los presentes Términos y Condiciones. <br>Sin perjuicio de lo anterior, KAUFMANN podrá terminar inmediatamente el contrato sin derecho a indemnización alguna para el usuario, en los siguientes casos:</p>
                 <ol style="list-style: lower-alpha;">
                     <li>Si el usuario incumple cualquiera de las obligaciones o viola cualquiera de las prohibiciones mencionadas en los presentes Términos y Condiciones;</li>
@@ -84,7 +84,7 @@
             </div>
             <!-- Declaraciones -->
             <div class="col-12">
-                <h3>Declaraciones</h3>
+                <h2>Declaraciones</h2>
                 <p>UNO) El usuario declara que no ejerce prácticas relacionadas con los delitos descritos en la Ley N° 20.393; que tiene y mantendrá un comportamiento recto, estricto y diligente en el cumplimiento de las normas sobre Prevención de Delitos en los términos de la Ley N° 20.393; y que adhiere y respeta la Política de Prevención de Delitos de KAUFMANN.</p>
                 <p>DOS) El Usuario declara estar en conocimiento que la aplicación WICAR ha sido ofertada por Kaufmann para ser instalada en vehículos marca Mercedes Benz. Que, ha sido él mismo quien ha solicitado a Kaufmann la instalación de WICAR en un vehículo de marca distinta a Mercedes Benz. Que, Kaufmann ha aceptado la instalación de la aplicación en dicho vehículo, en la medida que el usuario lo exima de responsabilidad por los daños de cualquier tipo que pueda sufrir el vehículo objeto de la instalación de la aplicación y que tengan su origen en tal circunstancia. En consecuencia, el Usuario libera expresamente a Comercial Kaufmann S.A. de cualquier daño directo o indirecto que se pueda producir en el vehículo de su propiedad que ha sido objeto de la instalación de la aplicación WICAR.</p>
             </div>
